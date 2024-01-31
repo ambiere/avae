6f94aeddbcfe1e74c0744bb105836fed524cf611
zhid0399123 <zhid0399123@gmail.com>
2024-01-31 15:50:46 +0300

    chore: configured husky and lint-staged pre-commit hook

---
+++ .husky/install.js
@@ -0,0 +1,8 @@
+// if (process.env.NODE_ENV === "production" || process.env.CI === "true") {
+//   process.exit(0)
+// }
+
+// ;(async () => {
+//   const husky = await import("husky")
+//   husky.install()
+// })()

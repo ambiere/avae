c5ee66e1f3cbc33b896a3fd6963d08fd8e57dbe6
zhid0399123 <zhid0399123@gmail.com>
2024-02-02 14:53:00 +0300

    test(index.test): peni test suite

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,11 @@
+const path = require("path")
 const { test } = require("tap")
+const replit = require("../index")
+const fixtures = path.join(__dirname, "fixtures")
 
-test("Should generate release.md file", async (t) => {
+//TODO
+
+test("should generate release.md file", async (t) => {
+  replit(fixtures + "/CHANGELOG.md", fixtures + "/RELEASE.md")
   t.pass()
 })

877c860187eeda47f62923021912e32ca89eca17
zhid0399123 <zhid0399123@gmail.com>
2024-02-02 14:52:16 +0300

    chore(bin): updated peni importation

--- src/bin.js
+++ src/bin.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
 const path = require("path")
-const relit = require("../index")
+const peni = require("../index")
 const commist = require("commist")()
 const argv = require("yargs-parser")(process.argv)
 const help = require("help-me")({ dir: path.join(path.dirname(require.main.filename), "..", "help") })
 
 commist.register("help", help.toStdout)
 commist.register("version", () => console.log(require("../package.json").version))
-commist.register("create", relit.cli)
+commist.register("create", peni.cli)
 
 if (argv.help) {
   help.toStdout()
 } else {
   if (argv._.length === 2) {
-    relit.cli(process.argv.splice(2))
+    peni.cli(process.argv.splice(2))
   } else commist.parse(process.argv.slice(2))
 }
